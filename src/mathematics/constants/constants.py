0864655ad23fcb1438ccd1499f96d4277501c6b8
Luke Stuemke <lstuemke93@gmail.com>
2018-07-22 01:04:47 -0400

    Initial commit. Created source structure. Created a class to calculate characteristic impedance and effective permittivity of coplanar strips transmission lines.

---
+++ src/mathematics/constants/constants.py
@@ -0,0 +1,8 @@
+from math import pi
+
+eps0 = 8.854187817620e-12
+mu0 = pi * 4e-7
+
+eta0 = (mu0 / eps0) ** 0.5
+
+
---
+++ src/mathematics/integrals/integrals.py
@@ -0,0 +1,9 @@
+from scipy.special import ellipk
+
+
+def elliptic_k(m):
+
+    return ellipk(m)
+
+
+
